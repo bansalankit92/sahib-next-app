a6019e263a65cf333de86fc99855c626c858334a
Ankit Bansal <ankit@antbrains.com>
2023-09-20 20:06:09 +0530

    rendered md files over network

---
+++ src/app/books/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, {useEffect, useState} from "react";
+import {marked} from "marked";
+
+
+interface PlayerProps {
+
+}
+
+const BooksSection: React.FC<PlayerProps> = ({}) => {
+
+    const [mdBook, setMdBook] = useState<string>();
+
+    useEffect(() => {
+
+        fetch("https://raw.githubusercontent.com/bansalankit92/indian-script-reader/master/assets/aavey-na-javey-na-marey/aavey-na-javey-na-janme-hin.md",)
+            .then(response => response.blob())
+            .then(buffer => {
+                var reader = new FileReader();
+                reader.readAsText(buffer, "ISO-8859-1")
+                reader.onload = function (e) {
+                    console.log(reader.result)
+                    setMdBook(reader.result as string)
+                }
+            })
+    }, []);
+
+    return (
+        <section
+            className="container mx-auto px-4 py-8 md:py-12 lg:py-16 xl:py-20 2xl:py-24 3xl:py-28 bg-white dark:bg-gray-900"
+            id="books-section">
+            <div
+                className="flex flex-col items-center justify-center w-full mb-8 md:mb-12 lg:mb-16 xl:mb-20 2xl:mb-24 3xl:mb-28">
+                <h2 className="text-3xl font-bold text-center text-gray-800 md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl 3xl:text-8xl">
+                    Books
+                </h2>
+                <p className="text-center text-gray-600 md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl">
+                    Read books and read more about Indian scripts.
+                </p>
+            </div>
+            <div>
+                {mdBook && <MarkdownRenderer markdown={mdBook}/>}
+            </div>
+        </section>
+    );
+}
+
+
+const MarkdownRenderer = ({markdown}: { markdown: string }) => {
+
+    function getMarkdownText() {
+        let rawMarkup = marked.parse(markdown);
+        return {__html: rawMarkup};
+    }
+
+    return (
+        <article className="md1 prose dark:prose-invert max-w-none">
+            <div dangerouslySetInnerHTML={getMarkdownText()}/>
+        </article>
+    )
+}
+
+export default BooksSection;
