20202ac4bfc2ebdc22d7d6b8ac19d526917f9219
Ankit Bansal <ankit.bansal@peerlist.io>
2024-04-13 19:07:11 +0530

    change env variables

--- src/db/connectDb.ts
+++ src/db/connectDb.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const DATABASE_URL = process.env.DATABASE_URL || '';
+const DATABASE_URL = process.env.MONGODB_URI || '';
 
 if (!DATABASE_URL) {
     throw new Error("Please define the DATABASE_URL environment variable inside .env.local");
